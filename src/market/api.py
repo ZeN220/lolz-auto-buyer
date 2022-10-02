bb73d6bbb0f14dc70810c36e9bd05cd6746528e9
ZeN <artaganmalsagov07@gmail.com>
2022-10-02 20:40:25 +0300

    fix parse data from search url

--- src/market/api.py
+++ src/market/api.py
@@ -14,12 +14,13 @@ class MarketAPI(BaseMarketAPI):
         return self._headers
 
     def search(self, category: str, search_params: str) -> Response:
-        response = self.api_request(f'{category}/?{search_params}')
+        response = self.api_request(f'{category}/{search_params}')
         return response
 
     @staticmethod
     def parse_search_data(search_url: str) -> Optional[Tuple[str, str]]:
-        parse = re.findall(r'https://lolz.guru/market/([\w\-]+)/?(.+)', search_url)
+        parse = re.search(r'https://lolz.guru/market/([\w\-]+)/(.+)', search_url)
         if not parse:
             raise TypeError('Format search URL is invalid')
-        return parse[0]
+        category, search_params = parse.groups()
+        return category, search_params
